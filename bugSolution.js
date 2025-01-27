dc4ce83d747b5028b8edd0efa4e7e160fba94fe8
BugHunter-X <askzenai@gmail.com>
2025-01-27 13:34:05 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,8 @@
+function foo(a, b) {
+  return Number(a) + Number(b);
+}
+
+console.log(foo(1, '1')); // Output: 2 (correct numerical addition)
+console.log(foo(1, 1)); //Output: 2
+console.log(foo('1', 1)); //Output: 2
+console.log(foo('1','1')); //Output: 2
